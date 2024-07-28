6848dfbd23f3e854d0207ecf04daaf6f8acde09b
rachel <rachelp7201@gmail.com>
2024-07-28 12:36:51 +0300

    lesson3,4 continue with auth

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
   constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate() {
+    return this.authService.isLoggedIn().pipe(
+      tap(userIsLoggedIn=>{
+        if(!userIsLoggedIn)
+          this.router.navigate(['/']);
+      })
+    )
+  }
 }
